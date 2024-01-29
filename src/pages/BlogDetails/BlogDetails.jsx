c58c4a488b540568701055e3072e8827a702bfdf
NieamulKabir <kabir000079@gmail.com>
2024-01-29 11:18:21 +0600

    load comment

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -1,19 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
+import BlogComment from "../../components/BlogComment/BlogComment";
 
 const BlogDetails = () => {
   let { id } = useParams();
 
-  const {
-    data: blogsDetails = {},
-    isLoading: loading,
-    refetch,
-  } = useQuery({
+  // get blogDetails by id
+  const { data: blogsDetails = {} } = useQuery({
     queryKey: ["blogDetails", id],
     queryFn: async () => {
       const res = await fetch(`http://localhost:5000/blogs/${id}`);
@@ -21,19 +19,7 @@ const BlogDetails = () => {
     },
   });
 
-  const { data: allComments = [] } = useQuery({
-    queryKey: ["allComments"],
-    queryFn: async () => {
-      const res = await fetch(`http://localhost:5000/comments`);
-      return res.json();
-    },
-  });
-  console.log(allComments.data);
-
-  const blogComments = allComments?.data?.filter(
-    (blogComment) => blogComment.blogId === id
-  );
-  console.log(blogComments);
+  //post comment in dataBase
   const { register, handleSubmit, reset } = useForm();
   const onSubmit = (data) => {
     const blogId = id;
@@ -46,9 +32,27 @@ const BlogDetails = () => {
       if (res.data.insertedId) {
         toast("Added successfully");
         reset();
+        refetch();
       }
     });
   };
+
+  //get all comment
+  const { data: allComments = [], refetch } = useQuery({
+    queryKey: ["allComments"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/comments`);
+      return res.json();
+    },
+  });
+ 
+
+  //filter comment by blogId
+  const blogComments = allComments?.data?.filter(
+    (blogComment) => blogComment.blogId === id
+  );
+
+
   return (
     <div className="pt-28 pb-10 text-white">
       <div className="card card-compact w-3/4 mx-auto bg-gray-800 shadow-xl">
@@ -64,15 +68,28 @@ const BlogDetails = () => {
           <p>{blogsDetails?.description}</p>
         </div>
 
-        <form  onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <input
             {...register("comment")}
             type="text"
             placeholder="Comment Here"
             className="input input-bordered md:w-full  md:max-w-sm mx-w-xs bg-gray-500 text-white mx-4 my-3"
           />
-          <input className="btn font-bold bg-gray-500 text-white hover:text-black ml-4 mb-4" type="submit" />
+          <input
+            className="btn font-bold bg-gray-500 text-white hover:text-black ml-4 mb-4"
+            type="submit"
+          />
         </form>
+      </div>
+      <div>
+        <h1 className="text-center text-4xl font-mono font-bold my-4">
+          Blog Comments
+        </h1>
+        <div>
+          {blogComments?.map((comment) => (
+            <BlogComment key={comment._id} comment={comment} />
+          ))}
+        </div>
       </div>
     </div>
   );
